a646d7da3aafc069d96bebd01e91568cc01a38bd
Conor Murnane <conormurnane11@gmail.com>
2020-11-06 21:37:06 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,40 @@
 # Write your code here!
+def game_hash
+  {
+    :home => {
+      :team_name => "",
+      :colors => [],
+      :players => [
+        {
+        :player_name => [""],
+        :number => "",
+        :shoe => "",
+        :points => "",
+        :rebounts => "",
+        :assists => "",
+        :blocks => "",
+        :slam_dunks => ""
+        }        
+        ]
+
+    },
+    :away =>{
+      :team_name => "",
+      :colors => [],
+      :players => [
+        {
+        :player_name => "",
+        :number => "",
+        :shoe => "",
+        :points => "",
+        :rebounts => "",
+        :assists => "",
+        :steals => "",
+        :blocks => "",
+        :slam_dunks => ""
+        }
+        ]
+    }
+    
+  }
+end
